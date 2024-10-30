e948fe91c4329a544f2467d2daa13273ab9e7d97
John Henry Nasong Torres <johnhenrynasongtorres11@gmail.com>
2024-10-30 16:00:37 +0800

    minor changes in ui and user authentication

--- js/firebaseauth.js
+++ js/firebaseauth.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-analytics.js";
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
-import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBcp4pT_gjNkNJV8PU8T2Fx2ahBblFLMEs",
-  authDomain: "skill-stack-df84c.firebaseapp.com",
-  projectId: "skill-stack-df84c",
-  storageBucket: "skill-stack-df84c.appspot.com",
-  messagingSenderId: "208320005858",
-  appId: "1:208320005858:web:ec4bf63c6da69c598680bb",
-  measurementId: "G-WMJQPKF15X"
+    apiKey: "AIzaSyBcp4pT_gjNkNJV8PU8T2Fx2ahBblFLMEs",
+    authDomain: "skill-stack-df84c.firebaseapp.com",
+    projectId: "skill-stack-df84c",
+    storageBucket: "skill-stack-df84c.appspot.com",
+    messagingSenderId: "208320005858",
+    appId: "1:208320005858:web:ec4bf63c6da69c598680bb",
+    measurementId: "G-WMJQPKF15X"
 };
 
 // Initialize Firebase
@@ -54,7 +54,7 @@ function showMessage(message, divId){
         const docRef=doc(db, "tbl_users", user.uid);
         setDoc(docRef,userData)
         .then(()=>{
-            window.location.href='login.html';
+            window.location.href='index.html';
         })
         .catch((error)=>{
             console.error("error writing document", error);
@@ -84,7 +84,7 @@ function showMessage(message, divId){
         showMessage('login is successful', 'signInMessage');
         const user=userCredential.user;
         localStorage.setItem('loggedInUserId', user.uid);
-        window.location.href='../index.html';
+        window.location.href='html/home.html';
     })
     .catch((error)=>{
         const errorCode=error.code;
---
+++ js/toggle-chat.js
@@ -0,0 +1,4 @@
+function toggleChatbox() {
+    const chatbox = document.getElementById('chatbox');
+    chatbox.style.display = chatbox.style.display === 'none' || chatbox.style.display === '' ? 'block' : 'none';
+}

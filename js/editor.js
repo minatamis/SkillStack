7d47c68a6cf372b12508ec5578e314bbb8563c14
Jem Cebuano <116412303+minatamis@users.noreply.github.com>
2024-11-07 20:28:40 +0800

    minor changes

---
+++ js/editor.js
@@ -0,0 +1,20 @@
+let editor;
+window.onload = function()
+{
+    editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/java");
+}
+
+function changeLaguage()
+{
+    let language = $("#languages").val();
+
+    if (language === 'java') {
+        editor.session.setMode("ace/mode/java");
+    } else if (language === 'csharp') {
+        editor.session.setMode("ace/mode/csharp");
+    } else if (language === 'python') {
+        editor.session.setMode("ace/mode/python");
+    }
+}
--- js/toggle-chat.js
+++ js/toggle-chat.js
@@ -2,3 +2,34 @@ function toggleChatbox() {
     const chatbox = document.getElementById('chatbox');
     chatbox.style.display = chatbox.style.display === 'none' || chatbox.style.display === '' ? 'block' : 'none';
 }
+
+class ChatHead extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="chatbot-icon" onclick="toggleChatbox()">
+            <img src="../assets/images/ava.gif" alt="Chatbot Icon"> 
+        </div>
+
+        <div class="chatbox" id="chatbox">
+            <div class="chatbox-header">
+                <h3>StackBot</h3>
+                <button onclick="toggleChatbox()">
+                    <img src="../assets/images/close.png" alt="Close">
+                </button>
+            </div>
+            <div class="chatbox-body">
+                <p>How may I help you today?</p>
+            </div>
+            <div class="chatbox-footer">
+                <input type="text" placeholder="Type your message...">
+                <button class="send-button">
+                    <img src="../assets/images/send.png" alt="Send" />
+                </button>
+            </div>
+            
+        </div>
+        `;
+    }
+}
+
+customElements.define('chat-head', ChatHead)
